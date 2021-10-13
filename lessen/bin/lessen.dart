9de81e60167e60b9fb7d017dca076de4bdce3bb1
unknown <erjanesengeldiev@gmail.com>
2021-10-13 20:05:32 +0600

    first commit

---
+++ lessen/bin/lessen.dart
@@ -0,0 +1,13 @@
+void main() {
+  var a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  var res = [];
+  var fdsf = [];
+  var c = [];
+  var fafadf = [];
+  for (int i = 0; i < a.length; i++) {
+    if (i.isEven) {
+      res.add(a[i]);
+    }
+  }
+  print(res);
+}

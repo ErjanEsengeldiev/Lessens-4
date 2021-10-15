624bf5db423ba3944544692f7b3bd29ddfab4cc1
unknown <erjanesengeldiev@gmail.com>
2021-10-15 13:02:02 +0600

    Demo dz

--- lessen/bin/lessen.dart
+++ lessen/bin/lessen.dart
@@ -1,13 +1,15 @@
 void main() {
   var a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-  var res = [];
-  var fdsf = [];
-  var c = [];
-  var fafadf = [];
-  for (int i = 0; i < a.length; i++) {
-    if (i.isEven) {
-      res.add(a[i]);
+  var b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  var e = [];
+
+  for (var c in a) {
+    for (var d in b) {
+      if (c == d) {
+        e.add(c);
+        for (var t in e) {}
+      }
     }
   }
-  print(res);
+  print(e);
 }
